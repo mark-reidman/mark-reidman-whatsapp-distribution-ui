9d4672d419e05a37e26df52840f9955ae8a100ee
Eli Popovitch <dataleaders@yandex.com>
2021-01-19 22:15:26 +0200

    Ui Clean

--- src/components/sections-page/HeadersSection.js
+++ src/components/sections-page/HeadersSection.js
@@ -17,10 +17,10 @@ function HeadersSection() {
     <>
       <div className="cd-section" id="headers">
         <Header1 />
-        <Header2 />
-        <Header3 />
-        <Header4 />
-        <Header5 />
+        {/*<Header2 />*/}
+        {/*<Header3 />*/}
+        {/*<Header4 />*/}
+        {/*<Header5 />*/}
       </div>
     </>
   );
